bd40bc7b712ba7aa9a2c9d4bd3ce2ec44f4e5537
Peter Laengert <peterlaengert@gmail.com>
2024-10-17 14:44:17 -0400

    Rebuild app

--- dist/cloudflare/_worker.js/main.server.mjs
+++ dist/cloudflare/_worker.js/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NBMVJN6D.mjs";import"./chunk-JOHBUDQX.mjs";import"./chunk-M3IHTRDF.mjs";export{a as default};
+import{a}from"./chunk-WVZWA6TC.mjs";import"./chunk-JOHBUDQX.mjs";import"./chunk-M3IHTRDF.mjs";export{a as default};
---
+++ dist/server/chunk-WVZWA6TC.mjs
@@ -0,0 +1,25 @@
+import './polyfills.server.mjs';
+import{B as u,a as m,b as e,c as t,d as i,e as n,f as a,k as c,n as l,o as s,p as f,q as g,r as C,u as b,y as d}from"./chunk-JOHBUDQX.mjs";var o=class p{title="tempwebsite";static \u0275fac=function(r){return new(r||p)};static \u0275cmp=m({type:p,selectors:[["app-root"]],standalone:!0,features:[a],decls:33,vars:0,consts:[[1,"main"],["align","center"]],template:function(r,M){r&1&&(e(0,"main",0)(1,"title"),n(2,"Newmarket BIC Church"),t(),e(3,"header")(4,"h1",1),n(5,"Newmarket BIC Church"),t()(),i(6,"br")(7,"br")(8,"br")(9,"br")(10,"br")(11,"br")(12,"br")(13,"br")(14,"br")(15,"br")(16,"br")(17,"br")(18,"br")(19,"br")(20,"br")(21,"br")(22,"br")(23,"br")(24,"br")(25,"br")(26,"br")(27,"br")(28,"br"),e(29,"footer")(30,"p",1),n(31,"Copyright \xA92024 Newmarket BIC Church"),t()()(),i(32,"router-outlet"))},dependencies:[d],styles:[`h1[_ngcontent-%COMP%] {
+    font-size: 3.125rem;
+    color: var(--gray-900);
+    font-weight: 500;
+    line-height: 100%;
+    letter-spacing: -0.125rem;
+    margin: 0;
+    font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+
+  p[_ngcontent-%COMP%] {
+    margin: 0;
+    color: var(--gray-700);
+  }
+
+  .content[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+    margin-top: 1.75rem;
+  }
+
+  .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+    margin-top: 1.5rem;
+  }`]})};var h=[];var v={providers:[s(f()),c({eventCoalescing:!0}),u(h),C()]};var S={providers:[b()]},A=l(v,S);var x=()=>g(o,A),J=x;export{J as a};
